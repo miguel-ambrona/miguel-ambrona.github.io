2835c5e21f171c252da360a5c0428218f8c91f79
ambrona <miguel.ambrona@imdea.org>
2020-03-03 22:52:23 +0900

    First commit

---
+++ chess/js/help.js
@@ -0,0 +1,164 @@
+// Functions to control the Graphical User Interface of the tool (Help document)
+
+$('#pnum-btn').click(function(){ showHelp(pnum_title,pnum_msg,'pnum'); });
+$('#hint-btn').click(function(){ showHelp(hint_title,hint_msg,'hint'); });
+$('#prev-btn').click(function(){ showHelp(prev_title,prev_msg,'prev'); });
+$('#rset-btn').click(function(){ showHelp(rset_title,rset_msg,'rset'); });
+$('#next-btn').click(function(){ showHelp(next_title,next_msg,'next'); });
+$('#eyes-btn').click(function(){ showHelp(eyes_title,eyes_msg,'eyes'); });
+$('#gear-btn').click(function(){ showHelp(gear_title,gear_msg,'gear'); });
+$('#dpth-btn').click(function(){ showHelp(dpth_title,dpth_msg,'dpth'); });
+$('#eval-btn').click(function(){ showHelp(eval_title,eval_msg,'eval'); });
+$('#rbot-btn').click(function(){ showHelp(rbot_title,rbot_msg,'rbot'); });
+$('#edit-btn').click(function(){ showHelp(edit_title,edit_msg,'edit'); });
+$('#shot-btn').click(function(){ showHelp(shot_title,shot_msg,'shot'); });
+$('#bell-btn').click(function(){ showHelp(bell_title,bell_msg,'bell'); });
+$('#tags-btn').click(function(){ showHelp(tags_title,tags_msg,'tags'); });
+$('#rand-btn').click(function(){ showHelp(rand_title,rand_msg,'rand'); });
+$('#bomb-btn').click(function(){ showHelp(bomb_title,bomb_msg,'bomb'); });
+$('#errs-btn').click(function(){ showHelp(errs_title,errs_msg,'errs'); });
+$('#tree-btn').click(function(){ showHelp(tree_title,tree_msg,'tree'); });
+$('#pass-btn').click(function(){ showHelp(pass_title,pass_msg,'pass'); });
+
+$('#book-btn').click(function(){ showHelp(book_title,book_msg,'book'); });
+$('#puzz-btn').click(function(){ showHelp(puzz_title,puzz_msg,'puzz'); });
+$('#endb-btn').click(function(){ showHelp(endb_title,endb_msg,'endb'); });
+$('#game-btn').click(function(){ showHelp(game_title,game_msg,'game'); });
+$('#help-btn').click(function(){ showHelp(help_title,help_msg,'help'); });
+
+function appearEffect(title) {
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.ml1 .letters');
+    textWrapper.innerHTML = title.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: false})
+        .add({
+            targets: '.ml1 .letter',
+            scale: [0.3,1],
+            opacity: [0,1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 600,
+            delay: (el, i) => (80 - 4*title.length) * (i+1)
+        });
+};
+
+function disolve(element, i){
+    if (i <= 10){
+        element.css('opacity', 0.1*i);
+        setTimeout(function(){ disolve(element, i+1) }, 70-10*i);
+    }
+}
+
+function showHelp(title, msg, btn) {
+    $('#helpTitle').css('visibility', 'hidden');
+    $('#helpMsg').css('opacity', 0);
+
+    var joined_msg = ''
+    for (var i = 0; i < msg.length; i++){
+        joined_msg += '<p>' + msg[i] + '</p>';
+        if (i < msg.length - 1){ joined_msg += '<hr>'; }
+    }
+
+    document.getElementById('helpMsg').innerHTML = joined_msg;
+    disolve($('#helpMsg'), 1);
+
+    appearEffect(title);
+    setTimeout(function(){ $('#helpTitle').css('visibility', 'visible'); }, 50);
+
+    $('.btn-help').removeClass('btn-help');
+    $('#' + btn + '-btn').addClass('btn-help');
+}
+
+pnum_title = 'Índice';
+pnum_msg   = ['Éste es el número identificativo del problema.',
+              'Al hacer <i>click</i> en este botón, se mostrará (durante un instante) el número total de problemas en el bloque de ejercicios.'];
+
+hint_title = 'Consejo';
+hint_msg   = ['Haz <i>click</i> para ver una pista sobre cómo proceder en la posición actual.',
+              'En el <i>modo robot</i> no se debe confiar ciegamente en la calidad del consejo, ya que éste se calcula mediante Stockfish a profundidad no muy elevada.'];
+
+prev_title = 'Problema anterior';
+prev_msg   = ['Este botón carga el problema anterior al actual.',
+              'Cuando el <i>modo edición libre</i> esté activo, este botón permitirá deshacer una jugada.'];
+
+rset_title = 'Repetir problema';
+rset_msg   = ['Vuelve a cargar el problema actual para resolverlo de nuevo.'];
+
+next_title = 'Siguiente problema';
+next_msg   = ['Este botón carga el problema siguiente al actual.',
+              'Cuando el <i>modo edición libre</i> esté activo, este botón permitirá rehacer una jugada.'];
+
+eyes_title = 'Ver evaluación';
+eyes_msg   = ['Permite mostrar u ocultar la evaluación de las posiciones (en aquéllas en las que esté disponible o haya sido calculada).'];
+
+gear_title = 'Motor';
+gear_msg   = ['Evalúa la posición actual con Stockfish. Tanto la evaluación como la profundidad de cálculo aparecerán a la izquierda del botón.',
+              'El primer análisis en cada posición se hace durante 1 segundo. A continuación, se puede incrementar la profundidad haciendo <i>click</i> en el botón de profundidad.'];
+
+dpth_title = 'Profundidad';
+dpth_msg   = ['Este botón indica la profundidad con la que se ha calculado la evaluación de la posición. Al hacer <i>click</i> en él, la posición se evaluará de nuevo con un grado más de profundidad.',
+              'Analizar posiciones a alta profundidad puede suponer un gasto energético alto, sobre todo para un teléfono móvil.'];
+
+eval_title = 'Evaluación';
+eval_msg   = ['Muestra la evaluación de la posición actual desde el punto de vista de las blancas.',
+              'El color de fondo será verde o rojo en función de si la posición favorece tu bando o no (respectivamente).',
+              'Al hacer <i>click</i> en este botón, se mostrará la mejor jugada propuesta por la evaluación.'];
+
+rbot_title = 'Modo robot';
+rbot_msg   = ['Este modo te permite jugar contra Stockfish en la posición actual del tablero.',
+              'Es especialmente útil para practicar a mantener la ventaja en posiciones en las que tienes dudas.'];
+
+edit_title = 'Modo edición libre';
+edit_msg   = ['Cuando este modo esté activado, podrás mover las piezas libremente en el tablero, para llegar a una posición de tu interés.',
+              'Este modo es incompatible con muchas funciones, que estarán desactivadas mientras esté activo.'];
+
+shot_title = 'Fotogramas';
+shot_msg   = ['Al hacer <i>click</i> en este botón, se mostrarán posiciones similares a la actual en las que la jugada recomendada es la misma.',
+              'Este modo es incompatible con muchas funciones, que estarán desactivadas mientras esté activo.'];
+
+bell_title = 'Campana';
+bell_msg   = ['Activa o desactiva el sonido de la campana que se reproduce al acertar un ejercicio.',
+              'La idea de dicho sonido es favorecer la memorización a través del sentido del oído.'];
+
+tags_title = 'Etiquetas';
+tags_msg   =  ['Este botón te permite saltar directamente a posiciones particularmente interesantes del bloque de problemas actual.',
+               'En el caso de estudio de aperturas, permite saltar a posiciones correspondientes a las variantes principales.'];
+
+rand_title = 'Modo aleatorio';
+rand_msg   = ['Cuando este modo esté activado, los problemas aparecerán en un orden aleatorio (en lugar de secuencial).',
+              'Practicar en este modo de vez en cuando es recomandable para evitar asociaciones entre problemas vecinos.'];
+
+bomb_title = 'Bombazos';
+bomb_msg   = ['Este botón te permite iterar por posiciones en las que hay un truco táctico o una jugada sorprendente (para tu bando).',
+              'Es recomendable estudiar estas posiciones con algo más de frecuencia para no dejar escapar oportunidades en tus partidas.'];
+
+errs_title = 'Corrije tus errores';
+errs_msg   = ['Haz <i>click</i> en este botón cuando muestre el símbolo <span class="fa fa-warning"></span> para resolver de nuevo posiciones en las que te has equivocado previamente.',
+              'Cuando resuelvas correctamente todas las posiciones con errores, el botón volverá al estado <span class="fa fa-check-circle"></span>.'];
+
+tree_title = 'Ramificación';
+tree_msg   = ['Este botón te permite controlar el nivel de ramificación de los problemas, para no distraerte con variantes más profundas.',
+              'Se ignorarán las posiciones cuyo número de jugadas desde la posición inicial es mayor que el indicado.', 'Haciendo <i>doble click</i> se elimina el límite.'];
+
+book_title = 'Modo aperturas';
+book_msg   = ['Modo diseñado para el estudio de aperturas.',
+              'Este botón abrirá una nueva ventana donde podrás seleccionar la apertura a estudiar.',
+              'En cada posición (aunque varias jugadas sean buenas) sólo hay una jugada válida, correspondiente a la variante prediseñada para el estudio.'];
+
+puzz_title = 'Modo táctica';
+puzz_msg   = ['Modo diseñado para el estudio de táctica.<br><br><b>Todavía en desarrollo.</b>'];
+
+
+endb_title = 'Modo finales';
+endb_msg   = ['Modo diseñado para el estudio de finales.<br><br><b>Aún no está disponible.</b>'];
+
+game_title = 'Modo partida';
+game_msg   = ['Cuando este modo esté activo, jugarás contra la máquina variantes completas del libro de aperturas seleccionado.', 'En este modo no se muestra la jugada prediseñada después de cada error.'];
+
+help_title = 'Ayuda';
+help_msg   = ['Pincha en cada botón para obtener información sobre sus funcionalidades.',
+              'Para volver al menú principal, pulsa en el link de <i>Volver</i> situado arriba a la derecha.'];
+
+
+showHelp(help_title, help_msg, 'help');
---
+++ js/board.js
@@ -0,0 +1,334 @@
+// NOTE: this code uses the chess.js library:
+// https://github.com/jhlywa/chess.js
+
+var board = null
+var game = new Chess()
+var $status = $('#status')
+var $fen = $('#fen')
+var $pgn = $('#pgn')
+
+var selectedPiece = null
+var promotionPiece = null;
+var variantIdx = 0;
+var solution   = null;
+var lastLoaded = 0;
+
+const piecesPath = 'img/chesspieces/Default/';
+const move_sound    = new Audio("audio/Move.mp3");
+const capture_sound = new Audio("audio/Capture.mp3");
+const bell_sound    = new Audio("audio/Bell.mp3");
+
+function candidateSquare(square) {
+    var squareEl = $('#myBoard .square-' + square);
+    if (squareEl.hasClass('black-3c85d') === true) { highlight = 'highlight-black'; }
+    else                                           { highlight = 'highlight-white'; }
+    squareEl.addClass(highlight);
+};
+
+function addHighlights(source) {
+  var moves = game.moves({
+      square: source,
+      verbose: true
+  });
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return;
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+      candidateSquare(moves[i].to);
+  }
+  $('#myBoard .square-' + source).addClass('highlight-candidate');
+};
+
+function removeHighlights() {
+    $('#myBoard .square-55d63').removeClass('highlight-white');
+    $('#myBoard .square-55d63').removeClass('highlight-black');
+    $('#myBoard .square-55d63').removeClass('highlight-candidate');
+};
+
+function clickOnPromotion(piece){
+    buildBoard(true, game.fen());
+    promotionPiece = piece[1].toLowerCase();
+    $('#myboard').find('.square-55d63').removeClass('promotion-background').removeClass('promotion-mask');
+    $('#prom0').remove();  $('#prom1').remove();  $('#prom2').remove();  $('#prom3').remove();
+}
+
+function popSelectPiece(target){
+    selectedPiece = null;
+    buildBoard(false, game.fen());
+    var piecesStyle = $('#myBoard').find('.piece-417db')[0].style;
+    var col = target[0];
+    var rows;
+    if (target[1] === '8'){ rows = [[8,'wQ'],[7,'wR'],[6,'wB'],[5,'wN']]; }
+    else { rows = [[1,'bQ'],[2,'bR'],[3,'bB'],[4,'bN']]; }
+    $('#myBoard').find('.square-55d63').addClass('promotion-mask');
+    for (i = 0; i < rows.length; i++){
+        var thisSquare = col + rows[i][0];
+        el = document.getElementsByClassName('square-' + thisSquare)[0];
+        img = '<img id="prom' + i + '" src="' + piecesPath + rows[i][1] + '.png" ';
+        img += 'style="width: ' + piecesStyle.width + '">';
+        link = '<a href="#myBoard" onClick="clickOnPromotion(\'' + rows[i][1] + '\')" ';
+        link += 'style="z-index:100">'  + img + '</a>';
+        el.innerHTML = link;
+        $('#myBoard').find('.square-' + thisSquare).addClass('promotion-background').removeClass('promotion-mask');
+    }
+}
+
+function askForPromotion(source, target, piece){
+   if (!promotionPiece) {
+       setTimeout(function(){ askForPromotion(source, target, piece); }, 300);
+
+   } else {
+       onDrop(source, target, piece);
+       onSnapEnd();
+   }
+};
+
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
+
+  // only pick up pieces for the side to move
+  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false
+  }
+  addHighlights(source);
+}
+
+function onClick (square, piece, position, orientation) {
+   removeHighlights();
+   if (selectedPiece){
+       onDrop(selectedPiece.loc, square, selectedPiece.piece);
+   }
+   onSnapEnd();
+}
+
+function onDrop (source, target, piece) {
+  removeHighlights();
+
+  // if source = target, a piece is selected for clicking move
+  if (source === target){
+     if (selectedPiece){
+        if (selectedPiece.loc === source){
+           selectedPiece = null;
+           return;
+        }
+     }
+     selectedPiece = { loc : source, piece : piece };
+     addHighlights(source);
+  }
+
+    var fen = game.fen();
+    var prom = 'q';
+    var waiting = 10;
+    var is_capture = false;
+    if (promotionPiece)   { prom = promotionPiece }
+    if (selectedPiece)    { waiting = 90; }
+    if (game.get(target)) { is_capture = true; }
+
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: prom
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+
+  // handle promotions
+  if ((target.substring(1,2) === '8' && piece === 'wP') ||
+      (target.substring(1,2) === '1' && piece === 'bP')) {
+      if (!promotionPiece){
+          game.load(fen);
+          setTimeout(function(){ popSelectPiece(target); }, waiting);
+          askForPromotion(source, target, piece);
+          return;
+      } else {
+          promotionPiece = null;
+      }
+  }
+
+  if (is_capture){ capture_sound.play(); } else { move_sound.play(); }
+  puzzleInteraction(move);
+}
+
+function highlightSquare(square, hcolor, ms) {
+    var squareEl = $('#myBoard .square-' + square);
+    if (squareEl.hasClass('black-3c85d') === true) { highlight = hcolor + '-black'; }
+    else                                           { highlight = hcolor + '-white'; }
+    squareEl.addClass(highlight);
+
+    setTimeout(function(){
+        $('#myBoard .white-1e1d7').removeClass(hcolor + '-white');
+        $('#myBoard .black-3c85d').removeClass(hcolor + '-black');
+    }, ms);
+};
+
+function automatic_move(move) {
+    var is_capture = false;
+    if (game.get(move.to)) { is_capture = true; }
+
+    game.move(move);
+    board.position(game.fen());
+    if (is_capture){ capture_sound.play(); } else { move_sound.play(); }
+}
+
+function undo_until_move() {
+    game.undo();
+    board.position(game.fen());
+
+    variantIdx++;
+    var move = solution[variantIdx];
+
+    setTimeout(function(){
+        if (move === 'undo'){ undo_until_move(); return; }
+        if (move === '-'){ variantIdx++; return; }
+
+        automatic_move({ from: move.substr(0,2), to: move.substr(2,2), promotion: move.substr(4,1) });
+        board.position(game.fen());
+        variantIdx++;
+        if (variantIdx >= solution.length){
+            finishPuzzle();
+        }
+        if (solution[variantIdx] === 'undo'){
+            setTimeout(function(){ undo_until_move(); }, 1000);
+        }
+    }, 800);
+};
+
+function puzzleInteraction(move) {
+    var m = move.from + move.to;
+    if (move.promotion){
+        m += move.promotion;
+    }
+
+    var suggested = solution[variantIdx];
+
+    if (suggested && m === suggested){
+        highlightSquare(move.from, 'suggestion', 700);
+        highlightSquare(move.to, 'suggestion', 700);
+        if (solution[variantIdx+1] === 'solved'){ finishPuzzle(); return; }
+    }
+
+    setTimeout(function(){
+        if (m === suggested){
+            if (variantIdx >= solution.length){ finishPuzzle(); return; }
+            variantIdx++;
+            var ans = solution[variantIdx];
+
+            if (!ans){ finishPuzzle(); return; }
+
+            if (ans === 'undo'){ undo_until_move(); return; }
+
+            automatic_move({ from: ans.substr(0,2), to: ans.substr(2,2), promotion: ans.substr(4,1) });
+            board.position(game.fen());
+            variantIdx++;
+            if (variantIdx >= solution.length){
+                setTimeout(function(){ finishPuzzle(); }, 700);
+            }
+            if (solution[variantIdx] === 'undo'){  console.log('hola'); undo_until_move(); return; }
+
+        } else {
+            game.undo();
+            board.position(game.fen());
+        }
+    }, 700);
+}
+
+function finishPuzzle() {
+    bell_sound.play();
+    setTimeout(function(){ loadPuzzle(lastLoaded+1); }, 1500);
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+   board.position(game.fen());
+}
+
+function loadPuzzle(i) {
+    if (i >= puzzles.length) { i = 0 };
+    $('#pz' + lastLoaded).removeClass('btn-primary').addClass('btn-secondary');
+    $('#pz' + i).removeClass('btn-secondary').addClass('btn-primary');
+
+    variantIdx = 0;
+    lastLoaded = i;
+    solution   = puzzles[i].solution.split(' ');
+    document.getElementById('puzzlegoal').innerHTML = puzzles[i].goal;
+
+    var fen = puzzles[i].fen;
+    game.load(fen);
+    if (game.turn() === 'b'){ board.orientation('black'); }
+    else { board.orientation('white'); }
+    board.position(fen);
+}
+
+function buildBoard(is_draggable, fen){
+  var config = {
+    draggable: is_draggable,
+    position: '8/8/8/8/8/8/8/8 w - - 0 1',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd,
+    appearSpeed: 'fast',
+    onMouseClickSquare: onClick,
+    pieceTheme: piecesPath + '{piece}.png'
+  };
+  game.load(fen);
+  board = Chessboard('myBoard', config);
+  if (game.turn() === 'b'){ board.orientation('black'); }
+  else { board.orientation('white'); }
+  board.position(fen, false);
+}
+
+var puzzles = [
+    // Paul Heüacker, <i>Neue Freie Presse</i> (1920)
+    { fen: '1B6/8/7P/4p3/3b3k/8/8/2K5 w - - 0 1',
+      solution: 'b8a7 d4a1 c1b1 a1c3 b1c2 c3a1 a7d4 e5d4 c2d3 undo undo a1d4 c2d3 h4g5 h6h7 d4a1 d3e4 undo undo g5g6 h7h8q e5e4 d3d4 solved',
+      goal : 'White to move'
+    },
+
+    // Leonid Kubbel, <i>Shakhmatny Listok</i> (1922)
+    { fen: '8/N7/K6B/3k4/3p4/p7/2PP4/8 w - - 0 1',
+      solution: 'a7c6 a3a2 c6b4 undo undo d5c6 h6g7 c6c5 g7f8 undo undo c6d5 d2d3 a3a2 c2c4 d4c3 g7c3 undo undo d5c5 a6b7 a2a1q g7f8 undo undo c5b4 g7d4 solved',
+      goal : 'White to move',
+    },
+
+    // J. Gunts, <i>Das Illustrierte Blatt</i> (1922)
+    { fen: '1NBk4/p2p4/8/3K4/8/8/8/8 w - - 0 1',
+      solution: 'c8b7 a7a5 b8a6 undo undo d8c7 b7a6 c7b8 d5d6 b8a8 d6c7 d7d5 a6b7 solved',
+      goal : 'White to move',
+    },
+
+    { fen: '8/1pPK3b/8/8/8/5k2/8/8 w - - 0 1',
+      solution: 'd7c8 b7b5 c8d7 b5b4 d7d6 h7f5 d6e5 f5c8 e5d4 b4b3 d4c3 c8e6 c7c8q e6c8 c3b3 solved',
+      goal : 'White to move'
+    },
+
+    { fen: '2N5/p7/q1p2p2/kp6/3P4/PP2K3/5P2/2B5 w - - 0 1',
+      solution: 'c1d2 b5b4 d2b4 a5b5 c8d6 b5b6 b4a5 a6a5 d6c4 undo undo b6a5 d6c4 a5b5 e3f4 a6c8 c4d6 undo undo c6c5 d4d5 f6f5 f4g5 f5f4 f2f3 solved',
+      goal : 'White to move',
+    },
+
+    { fen: '1kb5/1pNp4/1B1Pp3/P1p1p3/2p1P3/2PP4/3P1PKP/Q3R3 w - - 0 1',
+      solution: 'e1h1 c4d3 a1g1 c5c4 f2f4 e5f4 b6a7 solved',
+      goal : 'White to mate in 4',
+    },
+
+    { fen: '5r2/8/1R6/ppk3p1/2N3P1/P4b2/1K6/5B2 w - - 0 1',
+      solution: 'b6b5 c5b5 c4e5 b5b6 e5d7 undo undo b5a4 e5d7 f8c8 d7b6 undo undo f3e2 f1e2 f8b8 e2b5 b8b5 b2a2 solved',
+      goal : 'White to move',
+    },
+
+    { fen: '8/8/8/1P3r2/BpPk4/1p1b4/P5PP/R3K3 b Q c3 0 2',
+      solution: 'd4e3 a1d1 f5f1 undo undo e1c1 undo undo - b4c3 solved',
+      goal : 'Can black checkmate in 2 moves?',
+    }
+];
+
+buildBoard(true, '8/8/8/8/8/8/8/8 w - - 0 1');
+
+loadPuzzle(0);
